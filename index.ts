fc151a5b69aa270b492d5825151c676fd4afba3d
Quuuod <prizraklogan@gmail.com>
2023-05-07 13:21:53 +0300

    annotations

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-const str = 'str';
-console.log(str);
+// const str = 'str';
+// console.log(str);
+
+// let userName: string = 'Ivan';
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = 'John';
+
+age = +'40';
+
+if (isBirthday) {
+  console.log(`Happy birthday ${userName}, age: ${age + 1}`);
+}
