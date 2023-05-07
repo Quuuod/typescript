200d19a5578b3da8c695574fbe22291cb84ea4d8
Quuuod <prizraklogan@gmail.com>
2023-05-07 13:22:58 +0300

    tsc/ts-node

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var str = 'str';
-console.log(str);
+// const str = 'str';
+// console.log(str);
+// let userName: string = 'Ivan';
+var isBirthday = true;
+var age;
+var userName = 'John';
+age = +'40';
+if (isBirthday) {
+    console.log("Happy birthday ".concat(userName, ", age: ").concat(age + 1));
+}
