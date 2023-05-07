d240cccc8b61dda84dc3f88c1f043fbf973fd5b2
Quuuod <prizraklogan@gmail.com>
2023-05-07 16:47:03 +0300

    objects

--- index.ts
+++ index.ts
@@ -3,20 +3,29 @@
 
 // let userName: string = 'Ivan';
 
-const isBirthdayData: boolean = true;
-const userNameData: string = 'John';
-let ageData: number = 40;
+// const isBirthdayData: boolean = true;
+// const userNameData: string = 'John';
+// let ageData: number = 40;
 
-const logBrtMsg = (
-  isBirthday: boolean,
-  userName: string,
-  age: number
-): string => {
-  if (isBirthday) {
-    return `Happy birthday ${userName}, age: ${age + 1}`;
-  } else {
-    return 'Error';
-  }
-};
+// const logBrtMsg = (
+//   isBirthday: boolean,
+//   userName: string,
+//   age: number
+// ): string => {
+//   if (isBirthday) {
+//     return `Happy birthday ${userName}, age: ${age + 1}`;
+//   } else {
+//     return 'Error';
+//   }
+// };
 
-console.log(logBrtMsg(isBirthdayData, userNameData, ageData));
+// console.log(logBrtMsg(isBirthdayData, userNameData, ageData));
+
+const userData =
+  '{"isBirthDayData":true, "ageData": 40, "userNameData":"John"}';
+
+const userObj: {
+  isBirthdayData: boolean;
+  userNameData: string;
+  ageData: number;
+} = JSON.parse(userData);
