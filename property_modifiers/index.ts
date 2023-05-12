cf8e1da3f05d5286dfe765673433e033dd8a6466
Quuuod <prizraklogan@gmail.com>
2023-05-12 20:10:03 +0300

    property modifiers and generic

--- property_modifiers/index.ts
+++ property_modifiers/index.ts
@@ -12,7 +12,7 @@
 // };
 
 interface IUser {
-  login: string;
+  readonly login: string;
   password: string;
   age: number;
   addr: string | undefined; //обязательное, но может быть без значения
@@ -34,3 +34,28 @@ const dbName = '12345';
 function sendUserData(obj: IUser, db?: string): void {
   console.log(obj, db?.toLowerCase());
 }
+
+class Animal {
+  readonly name: string = 'name';
+}
+
+// const basicPorts: readonly number[] = [3000, 3001, 5555];
+
+// basicPorts[1] = 5554;
+// basicPorts.push(5545);
+
+const basicPorts: readonly [number, ...string[]] = [3000, '3001', '5555'];
+
+basicPorts[1] = 5554;
+basicPorts.push(5545);
+
+const basicPorts2: ReadonlyArray<number> = [3000, 3001, 5555]; //generic
+const userFreeze: Readonly<IUser> = {
+  login: 'login',
+  password: 'pass',
+  age: 50,
+  addr: '',
+};
+
+basicPorts2[1] = 5123;
+userFreeze.login = 'newLogin';
